b5385aa5d87f085e95203b4635b5f4653a2a1dc8
Aleksandar <aleksandar.gjorsoski@hotmail.com>
2020-09-14 19:42:08 +1000

    Push 3 14/09/2020

--- app/src/main/java/com/u3192633/centralexecutivefunctioncapturetool/DetailActivity.java
+++ app/src/main/java/com/u3192633/centralexecutivefunctioncapturetool/DetailActivity.java
@@ -2,13 +2,29 @@ package com.u3192633.centralexecutivefunctioncapturetool;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DetailActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        button = (Button) findViewById(R.id.detailContinue);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openinformation();
+            }
+        });
+    }
+    public void openinformation() {
+        Intent intent = new Intent(this, information.class);
+        startActivity(intent);
     }
 }

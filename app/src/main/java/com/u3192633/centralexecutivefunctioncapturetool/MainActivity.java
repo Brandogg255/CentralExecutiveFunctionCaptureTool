4a6f4bc307429513ce75dd7076e5dd11ea7d9b75
Aleksandar <aleksandar.gjorsoski@hotmail.com>
2020-09-22 19:13:57 +1000

    Push 3 14/09/2020

--- app/src/main/java/com/u3192633/centralexecutivefunctioncapturetool/MainActivity.java
+++ app/src/main/java/com/u3192633/centralexecutivefunctioncapturetool/MainActivity.java
@@ -9,11 +9,22 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
+    Button btn_sub;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_sub = (Button) findViewById(R.id.btn_sub);
+
+        btn_sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+                    public void onClick(View view) {
+                    Intent i = new Intent(getApplicationContext(), PopActivity.class);
+                    startActivity(i);
+            }
+        });
 
         button = (Button) findViewById(R.id.mainContinue);
         button.setOnClickListener(new View.OnClickListener() {
